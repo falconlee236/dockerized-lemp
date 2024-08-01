163b3a1526e8a340a377f3a8a68a89cf95c70e08
falconlee236 <falconlee236@cau.ac.kr>
2024-08-01 11:10:53 +0000

    start docker compose

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,17 +3,20 @@
 # start text
 echo start setting mariadb
 # check new version of package
-apt-get -y update
+apt-get -y -qq update
 # upgrade new version of package
-apt-get -y upgrade
+apt-get -y -qq upgrade
 # package install
-apt-get -y install mariadb-server vim
+apt-get -y -qq install mariadb-server vim
 
 # start mariadb server demon 
 # (mariadb는 mysql로 쉽게 변환 할수 있으며, 커멘드를 mysql로 통일함)
-service mysql start
-# .conf.d 파일에 bind-address줄을 주석처리 한다.(외부 ip에서 접근 허용)
-# -i option은 해당 파일에 변경사항을 적용하는 옵션
+# 이거는 왜 안되지...
+# service mysql start
+# .conf.d 파일에 bind-address줄을 주석처리 한다.(외부 ip에서 접근 허용) -i option은 해당 파일에 변경사항을 적용하는 옵션
 sed -i "s/bind-address/#bind-address/" /etc/mysql/mariadb.conf.d/50-server.cnf
 # debian config 파일에서 root 비밀번호 변경
 sed -i "s/password =/password = $MARIADB_ROOT_PWD/g" /etc/mysql/debian.cnf
+
+
+exec "$@"
